201c0e8532be65f8c28218111c68eb3fef6e07a3
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-09 16:13:50 +0530

    initialcommit

--- Physio-frontend/src/App.jsx
+++ Physio-frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      {/* You can add more routes here */}
     </Routes>
   );
 }
---
+++ Physio-frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+      <h1 className="text-5xl font-bold text-white">
+        🎉 Tailwind CSS is Working!
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
