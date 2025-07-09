68e2817575c58ff338f2d77e6fae0bfa22c77548
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-09 16:25:16 +0530

    initialcommit

--- Physio-frontend/src/components/Home.jsx
+++ Physio-frontend/src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
-      <h1 className="text-5xl font-bold text-white">
+    <ul class="bg-gradient-to-l-r from-blue-500 to-purple-600">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-l from-blue-500 to-purple-600 text-amber-300">
+      <h1 className="text-3xl  text-white font-bold">
         🎉 Tailwind CSS is Working!
       </h1>
     </div>
+    ,</ul>
   );
 }
 
