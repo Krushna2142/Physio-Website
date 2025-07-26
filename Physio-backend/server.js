6093ce8c7bdcb16653bec512384e9f53cecc49a4
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-26 11:58:34 +0530

    Initalcommit

--- Physio-backend/server.js
+++ Physio-backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://physio-website-estng9idk-krushna2142s-projects.vercel.app'
+}));
 app.use(express.json());
 
 // Helmet with CSP rules
