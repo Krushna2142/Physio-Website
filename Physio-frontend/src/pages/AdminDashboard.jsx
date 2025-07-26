70127d954eefa8bbad18b87e6e64e3e1ff7ca3a1
Krushna Ganesh Pokharkar <160628406+Krushna2142@users.noreply.github.com>
2025-07-26 14:08:31 +0530

    Solved problems

--- Physio-frontend/src/pages/AdminDashboard.jsx
+++ Physio-frontend/src/pages/AdminDashboard.jsx
@@ -15,7 +15,7 @@ export default function AdminDashboard() {
 
   return (
     <div className="flex flex-col items-center min-h-[70vh] bg-gradient-to-br from-indigo-50 to-purple-50 py-10">
-      <div className="bg-white shadow-2xl rounded-xl w-full max-w-3xl px-8 mt-10 py-10">
+      <div className="bg-white shadow-2xl rounded-xl w-full max-w-3xl px-8 mt-16 py-10">
         <h2 className="text-3xl font-extrabold text-indigo-700 mb-3 text-center">
           Welcome, Admin!
         </h2>
